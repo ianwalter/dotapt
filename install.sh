f5fef9aab3a3b301eec3f36bb97a4c1616bb5ed1
Ian Walter <public@iankwalter.com>
2018-11-02 14:23:25 -0400

    Adding Snapcraft support

--- install.sh
+++ install.sh
@@ -3,11 +3,26 @@
 # Copy .apt file to home directory.
 cp ./.apt ~/.apt
 
-# Install Homebrew packages from .brew file.
-packages=$(brew list)
+# Update repository cache.
+sudo apt update
+
+# Install Aptitude packages from .apt file.
+packages=$(apt list --installed 2>/dev/null | awk -F'/' 'NR>1{print $1}')
 while read p; do
   installed=$(echo "$packages" | grep -ce "^$p\$")
   if [ $installed == "0" ]; then
-    sudo apt install $p
+    sudo apt install -y $p
   fi
 done < ~/.apt
+
+# Copy .snapfile file to home directory.
+cp ./.snapfile ~/.snapfile
+
+# Install Snapcraft packages from .snap file.
+packages=$(snap list)
+while read p; do
+  installed=$(echo "$packages" | grep -ce "^$p\$")
+  if [ $installed == "0" ]; then
+    sudo snap install $p
+  fi
+done < ~/.snapfile
