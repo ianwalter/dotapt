6721f8165308d434d1f2ad90fa9f15035290ae30
Ian Walter <pub@ianwalter.dev>
2020-05-27 15:26:47 -0400

    Removing docker and adding build-essential and gcc for Homebrew

--- install.sh
+++ install.sh
@@ -6,18 +6,6 @@ cp ./.apt ~/.apt
 # Update repository cache.
 sudo apt update
 
-# Install packages that let apt use packages over HTTPS.
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-# Add Docker GPG key.
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Add Docker repository to apt sources.
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
-# Update the package database now that the Docker repo has been added.
-sudo apt update
-
 # Install Aptitude packages from .apt file.
 packages=$(apt list --installed 2>/dev/null | awk -F'/' 'NR>1{print $1}')
 while read p; do
@@ -38,17 +26,3 @@ while read p; do
     sudo snap install $p
   fi
 done < ~/.snapfile
-
-# Install Linuxbrew.
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-
-# Add user to docker group.
-if [[ $(cat /etc/group | grep docker) == '' ]]; then
-  sudo usermod -a -G docker $USER
-fi
-
-if [[ ! `which docker-compose` ]]; then
-  # Install up-to-date docker-compose.
-  sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-fi
